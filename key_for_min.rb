3f64a60a2dfa0348ac9a7e1d5d4433e4c5b8ca7d
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-19 18:27:30 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  winner = nil
+  current = 10000
+  name_hash.each do |key, value|
+    if key != nil && value < current
+      
+      current = value
+      winner = key
+    end
+  end
+  winner 
+  
 end
