0958f6585ed9f4845ce6fdfbcf32ee64d3d60de6
Vincent Collis <vinsanity0050@gmail.com>
2020-07-26 06:37:18 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,6 +9,24 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
+  if name_hash == nil
+    return nil
+  end
   
-  binding.pry
+  cache_min = nil
+  
+  name_hash.each do |key, value|
+    if cache_min == nil
+      cache_min = value
+    end
+    
+    if cache_min > value
+      cache_min = value
+    end
+  end
+  
+  name = name_hash.select {|name,value| value == cache_min}
+  name = name.flatten
+  name = name[0]
+  name
 end
